c551b3a788f6de6d3d6248abc83449d2344f1b46
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-07 07:26:30 +0700

    Add error handling in server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 import express from "express";
-const helmet = require("helmet");
 
 const app = express();
+
+// Error handling
+app.use("/", (err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal server error";
+  const stack = err.stack;
+
+  res.status(status).json({
+    success: false,
+    status: status,
+    message: message,
+    stack: stack,
+  });
+});
 
 app.listen(5000, () => {
   console.log("App is running at port 5000");

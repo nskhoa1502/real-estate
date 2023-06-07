6ba8546c0fdc4db1f8c19ebe3373ebff2df239ee
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-07 08:24:00 +0700

    Setup redux-toolkit, redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist/lib/storage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+let persistor = persistStore(store);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ client/src/store/store.js
@@ -0,0 +1,38 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { authReducer } from "../slices/authSlice";
+import { userReducer } from "../slices/userSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers({ auth: authReducer, user: userReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);

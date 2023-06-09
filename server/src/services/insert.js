6a23f6d48c58d7b614caf24485143a7eea3687e6
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-09 13:03:30 +0700

    backup commit

--- server/src/services/insert.js
+++ server/src/services/insert.js
@@ -19,7 +19,7 @@ export const insertService = async () => {
   try {
     dataBody.forEach((page) =>
       page.body.forEach(async (item) => {
-        let labelCode = generateCode(4);
+        let labelCode = generateCode(item?.header?.class?.classType);
         let postId = uuidv4();
         let attributesId = uuidv4();
         let userId = uuidv4();
@@ -51,9 +51,12 @@ export const insertService = async () => {
           id: imagesId,
           image: JSON.stringify(item?.images),
         });
-        await db.Label.create({
-          code: labelCode,
-          value: item?.header?.class?.classType,
+        await db.Label.findOrCreate({
+          where: { code: labelCode },
+          defaults: {
+            code: labelCode,
+            value: item?.header?.class?.classType,
+          },
         });
         await db.Overview.create({
           id: overviewId,
@@ -86,6 +89,86 @@ export const insertService = async () => {
         });
       })
     );
+    return "Insert data successfully";
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const insertService = async () => {
+  try {
+    for (const page of dataBody) {
+      for (const item of page.body) {
+        let labelCode = generateCode(item?.header?.class?.classType);
+        let postId = uuidv4();
+        let attributesId = uuidv4();
+        let userId = uuidv4();
+        let imagesId = uuidv4();
+        let overviewId = uuidv4();
+        await db.Post.create({
+          id: postId,
+          title: item?.header?.title,
+          star: item?.header?.star,
+          labelCode,
+          address: item?.header?.address,
+          attributesId,
+          categoryCode: "NCT",
+          description: JSON.stringify(item?.mainContent?.content),
+          userId,
+          overviewId,
+          imagesId,
+        });
+
+        await db.Attribute.create({
+          id: attributesId,
+          price: item?.header?.attributes?.price,
+          acreage: item?.header?.attributes?.acreage,
+          published: item?.header?.attributes?.published,
+          hashtag: item?.header?.attributes?.hashtag,
+        });
+
+        await db.Image.create({
+          id: imagesId,
+          image: JSON.stringify(item?.images),
+        });
+        await db.Label.findOrCreate({
+          where: { code: labelCode },
+          defaults: {
+            code: labelCode,
+            value: item?.header?.class?.classType,
+          },
+        });
+        await db.Overview.create({
+          id: overviewId,
+          code: item?.overview?.content.find((i) => i.name === "Mã tin:")
+            ?.content,
+          area: item?.overview?.content.find((i) => i.name === "Khu vực")
+            ?.content,
+          type: item?.overview?.content.find((i) => i.name === "Loại tin rao:")
+            ?.content,
+          target: item?.overview?.content.find(
+            (i) => i.name === "Đối tượng thuê:"
+          )?.content,
+          bonus: item?.overview?.content.find((i) => i.name === "Gói tin:")
+            ?.content,
+          created: item?.overview?.content.find((i) => i.name === "Ngày đăng:")
+            ?.content,
+          expired: item?.overview?.content.find(
+            (i) => i.name === "Ngày hết hạn:"
+          )?.content,
+        });
+
+        await db.User.create({
+          id: userId,
+          name: item?.contact?.content.find((i) => i.name === "Liên hệ:")
+            ?.content,
+          password: hashPassword("123456"),
+          phone: item?.contact?.content.find((i) => i.name === "Điện thoại:")
+            ?.content,
+          zalo: item?.contact?.content.find((i) => i.name === "Zalo")?.content,
+        });
+      }
+    }
     return "Insert data successfully";
   } catch (err) {
     throw err;
--- server/src/utils/generateCode.js
+++ server/src/utils/generateCode.js
@@ -1,12 +1,37 @@
-export const generateCode = (length) => {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
+require("dotenv").config;
+
+// export const generateCode = (length, secret) => {
+//   let result = "";
+//   let merge = value + process.env.SECRET_GENERATE;
+//   const characters =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   const charactersLength = characters.length;
+//   let counter = 0;
+//   while (counter < length) {
+//     let index = Math.floor(length / 2);
+//     result += merge.charAt(index);
+//     counter += 1;
+//   }
+//   return result;
+// };
+
+// Create a function that generate a certain value based on the value input
+export const generateCode = (value) => {
+  let output = "";
+  value = value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split(" ")
+    .join("");
+  let merge = value + process.env.SECRET_GENERATE;
+  let length = merge.length;
+  for (let i = 0; i < 4; i++) {
+    let index =
+      1 === i
+        ? Math.floor(merge.length / 2 + length / 2)
+        : Math.floor(length / 2);
+    output += merge.charAt(index);
+    length = index;
   }
-  return result;
+  return `${value.charAt(0)}${output}`.toUpperCase();
 };
